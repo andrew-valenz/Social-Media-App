b8491aabfba7b51da5339f3214dc72e46fc655ab
Aaron Ibarra <aaronibarra132@gmail.com>
2022-12-05 15:03:15 -0800

    grabbed DOM elements and made window load event listener

--- profile-editor/profile-editor.js
+++ profile-editor/profile-editor.js
@@ -1,5 +1,31 @@
 // import '../auth/user.js';
 import { getUser, upsertProfile } from '../fetch-utils.js';
 
+const profileForm = document.getElementById('profile-form');
+const updateBtn = profileForm.querySelector('button');
+const userNameInput = profileForm.querySelector('[name=username]');
+const avatarInput = profileForm.querySelector('[name=avatar]');
+const bioInput = profileForm.querySelector('[name=bio]');
+const errorDisplay = document.getElementById('error-display');
+
+let error = null;
+let profile = null;
+
 const user = getUser();
-console.log(user.data);
+
+window.addEventListener('load', async () => {
+    const response = await getProfile(user.id);
+    error = response.error;
+    profile = response.data;
+
+    if (error) {
+        errorDisplay.textContent = error.message;
+    } else {
+        if (profile) {
+            userNameInput.value = profile.username;
+        }
+        if (profile.bio) {
+            bioInput.value = profile.bio;
+        }
+    }
+});
