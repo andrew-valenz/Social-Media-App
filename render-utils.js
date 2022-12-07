301c7fca18c5e3fa25f0deeeaa5822dd6f3db4cc
Aaron Ibarra <aaronibarra132@gmail.com>
2022-12-07 13:28:14 -0800

    added render function for messages

--- render-utils.js
+++ render-utils.js
@@ -22,3 +22,49 @@ export function renderProfile(profileObject) {
     div.append(a, img, p);
     return div;
 }
+
+export function renderMessages(profile) {
+    const ul = document.createElement('ul');
+    const header = document.createElement('h3');
+
+    header.textContent = `Message Feed for ${profile.username}`;
+
+    ul.classList.add('messages');
+
+    ul.append(header);
+    //for (let message of profile.messages)
+    // substitute "message" for "profile.messages[i]"
+    for (let i = 0; i < profile.messages.length; i++) {
+        const li = document.createElement('p');
+        li.classList.add('message');
+
+        const div = document.createElement('div');
+        div.classList.add('message-info');
+
+        const senderSpan = document.createElement('span');
+        senderSpan.classList.add('from');
+        senderSpan.textContent = profile.messages[i].from_user;
+
+        const dateSpan = document.createElement('span');
+        dateSpan.classList.add('created-date');
+        dateSpan.textContent = new Date(profile.messages[i].created_at).toLocaleString('en-US', {
+            day: 'numeric',
+            month: 'numeric',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+        });
+
+        const text = document.createElement('p');
+        text.classList.add('text');
+        text.textContent = profile.messages[i].text;
+
+        div.append(senderSpan, dateSpan);
+
+        li.append(div, text);
+
+        ul.append(li);
+    }
+
+    return ul;
+}
